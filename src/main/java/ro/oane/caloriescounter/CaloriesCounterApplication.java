5209b6f6e0219161c61c9dd88d12fce9d941f8b0
ionut.pic <ionut.popa@esolutions.ro>
2023-07-12 17:53:54 +0300

    Initial commit - database properties - dummy index page

---
+++ src/main/java/ro/oane/caloriescounter/CaloriesCounterApplication.java
@@ -0,0 +1,13 @@
+package ro.oane.caloriescounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaloriesCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaloriesCounterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/oane/caloriescounter/CaloriesCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.oane.caloriescounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaloriesCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
